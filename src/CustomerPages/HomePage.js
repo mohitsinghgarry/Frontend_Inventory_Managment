40e5ad33ba83271550d6c3ba4652b7256a54a0ca
Mohit Singh <mohitsinghx4@gmail.com>
2024-11-01 16:09:10 +0530

    homepage content added

--- src/CustomerPages/HomePage.js
+++ src/CustomerPages/HomePage.js
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import HeroSection from '../CustomerPages/HeroSection'
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className='home-content'>
+    <HeroSection/>
+    </div>
   )
 }
 
